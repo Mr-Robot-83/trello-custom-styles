21cf81924e2e32e8a18517fbef5dfef30fa9a0ad
Mr-Robot-83 <scott.bonanno@gmail.com>
2022-03-18 00:13:34 +1100

    trying to update to manifest 3 :(

--- background.js
+++ background.js
@@ -1,36 +1,13 @@
-//listen for trello card page load
-chrome.tabs.onUpdated.addListener(
-  callback: function,
-)
+// background.js
 
-//find a position for the controller button
-let position = document.getElementsByClassName("window-header js-card-detail-header")[0];
+chrome.runtime.onInstalled.addListener(() => {
+  // default state goes here
+  // this runs ONE TIME ONLY (unless the user reinstalls your extension)
+});
 
-//create a div for the button to sit in
-let sidebarControllerContainer = document.createElement("div");
-sidebarControllerContainer.classList.add("window-header-inline-content");
-
-//place the div on the page
-position.append(sidebarControllerContainer);
-
-//place the link in the div
-sidebarControllerContainer.innerHTML = '<a class="quiet" href="#" id="sidebarController">Hide Sidebar</a>';
-
-//create a variable for the link
-let sidebarController = document.getElementById("sidebarController");
-
-//funtion to run on click
-sidebarController.onclick = function () {
-  let windowSidebar = document.getElementsByClassName("window-sidebar")[0];
-  let window = document.getElementsByClassName("window")[0];
-
-  if (windowSidebar.style.display === "none") {
-    windowSidebar.style.display = "";
-    window.style.width = "";
-    sidebarController.innerHTML = "Hide Sidebar"
-  } else {
-    windowSidebar.style.display = "none";
-    window.style.width = "600px";
-    sidebarController.innerHTML = "Show Sidebar"
-  }
-};
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tabId },
+    files: ["trello.js"]
+  })
+});
---
+++ trello.js
@@ -0,0 +1,31 @@
+//find a position for the controller button
+let position = document.getElementsByClassName("window-header js-card-detail-header")[0];
+
+//create a div for the button to sit in
+let sidebarControllerContainer = document.createElement("div");
+sidebarControllerContainer.classList.add("window-header-inline-content");
+
+//place the div on the page
+position.append(sidebarControllerContainer);
+
+//place the link in the div
+sidebarControllerContainer.innerHTML = '<a class="quiet" href="#" id="sidebarController">Hide Sidebar</a>';
+
+//create a variable for the link
+let sidebarController = document.getElementById("sidebarController");
+
+//funtion to run on click
+sidebarController.onclick = function () {
+  let windowSidebar = document.getElementsByClassName("window-sidebar")[0];
+  let window = document.getElementsByClassName("window")[0];
+
+  if (windowSidebar.style.display === "none") {
+    windowSidebar.style.display = "";
+    window.style.width = "";
+    sidebarController.innerHTML = "Hide Sidebar"
+  } else {
+    windowSidebar.style.display = "none";
+    window.style.width = "600px";
+    sidebarController.innerHTML = "Show Sidebar"
+  }
+};
