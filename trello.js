d98efbefe2ac3b1facdb581ee7cd69ef8acce5b0
Mr-Robot-83 <scott.bonanno@gmail.com>
2022-03-18 17:51:12 +1100

    hide last update message

--- trello.js
+++ trello.js
@@ -6,6 +6,10 @@ windowSidebarPanel.style.display = "none";
 var windowFrame = document.getElementsByClassName("window")[0];
 windowFrame.style.width = "600px";
 
+//remove last update
+var lastUpdate = document.getElementsByClassName("card-detail-item js-card-detail-age")[0];
+lastUpdate.style.display = "none";
+
 //find a position for the controller button
 var position = document.getElementsByClassName("window-header js-card-detail-header")[0];
 
@@ -27,10 +31,12 @@ sidebarController.onclick = function () {
   if (windowSidebarPanel.style.display === "none") {
     windowSidebarPanel.style.display = "";
     windowFrame.style.width = "";
+    lastUpdate.style.display = "";
     sidebarController.innerHTML = "Hide Sidebar"
   } else {
     windowSidebarPanel.style.display = "none";
     windowFrame.style.width = "600px";
     sidebarController.innerHTML = "Show Sidebar"
+    lastUpdate.style.display = "none";
   };
 };
